a10627d2502ed87223577b20c6f8d8aafd0e950e
rpathway <robbieyamashita1@byupathway.edu>
2025-11-06 17:29:49 -0500

    Week02 activities

---
+++ week02/scripts/bom.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button')
+const list = document.querySelector('#list');
+
+
+
+
+button.addEventListener('click', () => {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    li.textContent = input.value;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+
+    li.append(deleteButton);
+    list.append(li)
+
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+      input.focus();
+      input.value = ''
+    })
+   
+    input.value = ''
+  }
+  input.focus()
+})
